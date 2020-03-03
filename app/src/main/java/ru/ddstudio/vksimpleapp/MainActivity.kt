443fad06fd9171824a303d3b1ebfb791625440e9
DESKTOP-D2V1UJK\Danilshik <danilshik@yandex.ru>
2020-03-03 15:17:44 +0300

    Инициализация приложения

---
+++ app/src/main/java/ru/ddstudio/vksimpleapp/MainActivity.kt
@@ -0,0 +1,19 @@
+package ru.ddstudio.vksimpleapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.ddstudio.vksimpleapp.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+}
